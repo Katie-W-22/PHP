2f45bf0d33e5a3b25d1301e4dfffdc0a7d179c0a
“Katie-W-22” <katiecoder22@gmail.com>
2024-04-03 12:18:17 +0100

    basic feedback app complete

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -1,22 +1,33 @@
 <?php include 'includes/header.php' ?>
+
+<?php
+
+$sql = 'SELECT * FROM feedback';
+
+$result = mysqli_query($conn, $sql);
+
+$feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+?>
    
     <h3 style="color: lightblue;">Feedback Received!</h3>
+    
+    <?php if(empty($feedback)): ?>
+      <p>No feedback found</p>
+      <?php endif; ?>
+
     <img src="/php-katie/feedback/img/feedback1.png" class="w-50 mb-3" alt="">
-    <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
 
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
-
-   <div class="card my-3">
-     <div class="card-body">
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta molestias animi earum eos dolorem repellat a quibusdam, aperiam vero repellendus voluptatibus natus deserunt sed doloribus inventore, totam labore maxime perferendis!
-     </div>
-   </div>
+    <?php foreach($feedback as $item): ?>
+        <div class="card my-3 w-75">
+          <div class="card-body text-center">
+            <?php echo $item['body']; ?>
+            <div class="text-secondary mt-2">
+              from <?php echo $item['name']; ?>
+              on <?php echo $item['date']; ?>
+            </div>    
+          </div>
+        </div>
+    <?php endforeach; ?>   
+   
 <?php include 'includes/footer.php' ?>
